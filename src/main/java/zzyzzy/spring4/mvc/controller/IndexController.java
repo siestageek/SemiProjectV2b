3a0ab0dd7a22d5d1fa8172e4237ca124ea1445de
abc123 <abc123@xyz987.com>
2022-01-11 11:11:57 +0900

    세미프로젝트v2 첫번째 커밋

---
+++ src/main/java/zzyzzy/spring4/mvc/controller/IndexController.java
@@ -0,0 +1,19 @@
+package zzyzzy.spring4.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(Model m) {
+
+        m.addAttribute(
+               "msg", "Hello, World!!");
+
+        return "index";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title> ${msg} </title>
+</head>
+<body>
+<h1> ${msg} </h1>
+
+</body>
+</html>
